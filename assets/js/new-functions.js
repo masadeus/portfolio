d0c5f4a332bf8d83c46de2b4871b834a613fca84
Albert Mas <almaslac@gmail.com>
2022-09-15 18:46:58 +0200

    changes with script not working

--- assets/js/new-functions.js
+++ assets/js/new-functions.js
@@ -5,7 +5,7 @@ for ( const section of sections) {
     section.addEventListener( 'click', event => {
         if ( event.target.classList.value === 'transform-link' ) {
             section.querySelector( '.project' ).classList.toggle( 'shown' );
-            section.querySelector( '.web-design__visual' ).classList.toggle( 'move-out-right' );
+            section.querySelector( '.web-design__visual' ).classList.toggle( 'move-out-side' );
             if ( section.querySelector( '.project' ).classList.contains('shown') ) {
                 console.log("it is happening")
                 section.querySelector( '.transform-link' ).innerText = "Seen enough!"
