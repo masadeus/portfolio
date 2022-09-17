0d85bdfcf5e3c3ea10564ba6743b954919b3cfc1
Albert Mas <almaslac@gmail.com>
2022-09-17 14:05:34 +0200

    automatli portada

--- assets/js/new-functions.js
+++ assets/js/new-functions.js
@@ -4,11 +4,14 @@ const sections = document.querySelectorAll( "section" );
 for ( const section of sections) {
     section.addEventListener( 'click', event => {
         if ( event.target.classList.value === 'transform-link' ) {
-            section.querySelector( '.project' ).classList.toggle( 'shown' );
-            section.querySelector( '.web-design__visual' ).classList.toggle( 'move-out-side' );
+            section.querySelector( '.project' ).classList.toggle( 'shown' )
+            section.querySelector( '.web-design__visual' ).classList.toggle( 'move-out-side' )
+
             if ( section.querySelector( '.project' ).classList.contains('shown') ) {
                 section.querySelector( '.transform-link' ).innerText = "Seen enough!"
+                section.scrollIntoView( { block: "end", inline: "start" } )
             } else {
+
                 section.querySelector( '.transform-link' ).innerText = "Check it out"
             }
         }
