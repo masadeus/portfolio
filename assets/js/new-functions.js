93ba734fad953dd8f25ac7d7900cf43275ee8bb7
Albert Mas <almaslac@gmail.com>
2022-09-21 14:01:44 +0200

    js for scroling in smal devices

--- assets/js/new-functions.js
+++ assets/js/new-functions.js
@@ -8,12 +8,14 @@ for ( const section of sections) {
             if ( window.innerWidth >= 767 ) {
                 section.querySelector( '.web-design__visual' ).classList.toggle( 'move-out-side' )
             }
-
             if ( section.querySelector( '.project' ).classList.contains('shown') ) {
                 section.querySelector( '.transform-link' ).innerText = "Seen enough!"
-                section.scrollIntoView( { block: "end", inline: "start" } )
+                if ( window.innerWidth >= 767 ) {
+                    section.scrollIntoView( { block: "end", inline: "start" } )
+                } else {
+                    section.querySelector( ".web-design__content" ).scrollIntoView( { block: "start", inline: "end" } )
+                }
             } else {
-
                 section.querySelector( '.transform-link' ).innerText = "Check it out"
             }
         }
